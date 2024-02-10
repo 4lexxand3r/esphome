383d02b1f5dd18e689088effd9ade085bf998764
4lexxand3r <45030073+4lexxand3r@users.noreply.github.com>
2024-02-10 15:30:22 +0100

    2

--- esphome/components/gp8403_2/gp8403_2.h
+++ esphome/components/gp8403_2/gp8403_2.h
@@ -4,24 +4,24 @@
 #include "esphome/core/component.h"
 
 namespace esphome {
-namespace gp8403_2_2 {
+namespace gp8403_2 {
 
-enum gp8403_2_2Voltage {
-  gp8403_2_2_VOLTAGE_5V = 0x00,
-  gp8403_2_2_VOLTAGE_10V = 0x11,
+enum gp8403_2Voltage {
+  gp8403_2_VOLTAGE_5V = 0x00,
+  gp8403_2_VOLTAGE_10V = 0x11,
 };
 
-class gp8403_2_2 : public Component, public i2c::I2CDevice {
+class gp8403_2 : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  void set_voltage(gp8403_2_2::gp8403_2_2Voltage voltage) { this->voltage_ = voltage; }
+  void set_voltage(gp8403_2::gp8403_2Voltage voltage) { this->voltage_ = voltage; }
 
  protected:
-  gp8403_2_2Voltage voltage_;
+  gp8403_2Voltage voltage_;
 };
 
-}  // namespace gp8403_2_2
+}  // namespace gp8403_2
 }  // namespace esphome
